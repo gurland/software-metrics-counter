eec36aaf2c9c1ddf8c50535315983e0116b3691c
gurland <stas.gurland@gmail.com>
2023-04-27 06:07:41 +0300

    refactor: move args parser into a function

--- cli.py
+++ cli.py
@@ -1,12 +1,9 @@
 import argparse
 
-# Initialize parser
-parser = argparse.ArgumentParser(description="User")
-parser.add_argument("repository_link", help="Remote Git repository URL")
-parser.add_argument("-d", "--directories", nargs="+", help="Directories to analyze")
-parser.add_argument("-e", "--extensions", nargs="+", choices=[".c"], help="File extension to analyze")
-parser.parse_args()
 
-
-def get_source_code(git_repo_url: str):
-    raise NotImplementedError
+def parse_cli_args():
+    parser = argparse.ArgumentParser(description="This tool is used to analyze software metrics of any git repo.")
+    parser.add_argument("-repository_link", default="https://github.com/pallets/flask.git", help="Remote Git repository URL")
+    parser.add_argument("-d", "--directories", default=["src"], nargs="+", help="Repository directories to analyze")
+    parser.add_argument("-e", "--extensions", default=[".py"], nargs="+", choices=[".py"], help="File extensions to analyze")
+    return parser.parse_args()
