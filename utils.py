005d6a97b222733034d7837773d2eef0601a575e
gurland <stas.gurland@gmail.com>
2023-04-27 06:51:54 +0300

    feat: add plotly to render source code base statistics

--- utils.py
+++ utils.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import pygit2
+import plotly.graph_objects as go
 
 
 def get_source_code(git_repo_url: str) -> TemporaryDirectory:
+    print("Downloading git repository")
     temp_directory = TemporaryDirectory()
     pygit2.clone_repository(url=git_repo_url, path=temp_directory.name)
     return temp_directory
@@ -19,3 +21,40 @@ def get_source_file_paths_to_analyze(sources_path: Path, directories: list[str],
             )
 
     return file_paths_to_analyze
+
+
+def render_repository_metrics(repository_metrics):
+    headers = {
+        "values": [
+            "File Path",
+            "LOC",
+            "Empty LOC",
+            "Physical LOC",
+            "Logical LOC",
+            "Comment Lines",
+            "Comment Level (F)",
+            "Highest CC function",
+        ]
+    }
+
+    cells = {
+        "values": []
+    }
+
+    for header in headers["values"]:
+        row = []
+
+        for file_path, metrics in repository_metrics.items():
+            row.append(metrics[header])
+
+        cells["values"].append(row)
+
+    fig = go.Figure(
+        data=[
+            go.Table(
+                header=headers,
+                cells=cells
+            )
+        ]
+    )
+    fig.show()
